3ba8ff6f33889d167201c73f626bd9e531aaf0d4
inmount <inmount@live.cn>
2023-02-11 10:07:49 +0800

    修改：兼容配置参数

--- Suyaa.Microservice/WebHost.cs
+++ Suyaa.Microservice/WebHost.cs
@@ -1,4 +1,6 @@
-﻿namespace Suyaa.Microservice
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace Suyaa.Microservice
 {
     /// <summary>
     /// Web服务
@@ -16,6 +18,23 @@
             => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(
                 webBuilder =>
                 {
+                    webBuilder.UseStartup<TStartup>();
+                });
+
+        /// <summary>
+        /// 创建WehHost
+        /// </summary>
+        /// <typeparam name="TStartup"></typeparam>
+        /// <param name="configure"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder<TStartup>(Action<IWebHostBuilder> configure, string[]? args = null)
+            where TStartup : class
+            => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(
+                webBuilder =>
+                {
+                    // 执行配置
+                    configure(webBuilder);
                     webBuilder.UseStartup<TStartup>();
                 });
     }
